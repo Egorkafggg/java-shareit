f6553a5e90289328c321da477e02995cd5badcae
Егорка <159724676+Egorkafggg@users.noreply.github.com>
2026-07-06 19:36:25 +0300

    Add files via upload

--- src/main/java/ru/practicum/shareit/requests/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/requests/ItemRequestController.java
@@ -1,14 +1,12 @@
-package ru.practicum.shareit.booking;
+package ru.practicum.shareit.requests;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 /**
  * // TODO .
  */
 @RestController
-@RequestMapping(path = "/bookings")
-public class BookingController {
-
+@RequestMapping(path = "/requests")
+public class ItemRequestController {
 }
--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,12 +1,56 @@
 package ru.practicum.shareit.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.dto.UserDto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import java.util.List;
+
+@Slf4j
+@Validated
 @RestController
 @RequestMapping(path = "/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+    // УДАЛИТЕ: private final CheckConsistencyService checker;
+
+    @GetMapping
+    public List<UserDto> getUsers() {
+        log.info("Получен GET-запрос к эндпоинту: '/users' на получение всех пользователей");
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    public UserDto getUserById(@PathVariable Long userId) {
+        log.info("Получен GET-запрос к эндпоинту: '/users/{}' на получение пользователя", userId);
+        return userService.getUserById(userId);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDto create(@Valid @RequestBody UserDto userDto) {
+        log.info("Получен POST-запрос к эндпоинту: '/users' на добавление пользователя с email='{}'",
+                userDto.getEmail());
+        return userService.create(userDto);
+    }
+
+    @PatchMapping("/{userId}")
+    public UserDto update(@Valid @RequestBody UserDto userDto, @PathVariable Long userId) {
+        log.info("Получен PATCH-запрос к эндпоинту: '/users/{}' на обновление пользователя", userId);
+        return userService.update(userDto, userId);
+    }
+
+    @DeleteMapping("/{userId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long userId) {
+        log.info("Получен DELETE-запрос к эндпоинту: '/users/{}' на удаление пользователя", userId);
+        userService.delete(userId);
+        // УДАЛИТЕ: checker.deleteItemsByUser(userId);
+        // Теперь удаление вещей происходит через каскадное удаление в БД
+    }
 }
