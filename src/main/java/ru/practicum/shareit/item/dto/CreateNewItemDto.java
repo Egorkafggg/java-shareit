14c28c8908f17e21626a0a12b561b9ee462be11b
Егорка <159724676+Egorkafggg@users.noreply.github.com>
2026-07-06 18:32:55 +0300

    tz14

--- src/main/java/ru/practicum/shareit/item/dto/CreateNewItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/CreateNewItemDto.java
@@ -1,21 +1,23 @@
 package ru.practicum.shareit.item.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.experimental.FieldDefaults;
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class ItemDto {
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+@EqualsAndHashCode(of = {"id"})
+public class CreateNewItemDto {
     Long id;
     @NotBlank(message = "Наименование предмета бронирования не должно быть пустым")
     String name;
     @NotBlank(message = "Описание предмета бронирования не должно быть пустым")
     String description;
-    Boolean available = Boolean.FALSE;
+    @NotNull(message = "Нужно указать состояние предмета бронирования: он доступен или занят?")
+    Boolean available;
     Long owner;
     Long request;
 }
--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,23 @@
 package ru.practicum.shareit.item.model;
 
-/**
- * TODO Sprint add-controllers.
- */
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@EqualsAndHashCode(of = {"id"})
 public class Item {
+    Long id;
+    @NotBlank(message = "Наименование предмета бронирования не может быть пустым")
+    String name;
+    @NotBlank(message = "Описание предмета бронирования не должно быть пустым")
+    String description;
+    @NotNull(message = "Нужно указать статус предмета бронирования: он занят или свободен")
+    Boolean available;
+    Long owner;
+    Long request;
 }
