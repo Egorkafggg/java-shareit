fa6b35a91498a6aeb83c7d86bd0a7901422b4daf
Egorka <egorka.fggg@yandex.ru>
2026-07-11 23:31:53 +0300

    I hope that this last commit,plz

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,5 +1,7 @@
 package ru.practicum.shareit.item.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,9 +15,16 @@ import java.util.List;
 @NoArgsConstructor
 public class ItemDto {
     private Long id;
+
+    @NotBlank(message = "Name cannot be empty")
     private String name;
+
+    @NotBlank(message = "Description cannot be empty")
     private String description;
+
+    @NotNull(message = "Available must be specified")
     private Boolean available;
+
     private BookingInfoDto lastBooking;
     private BookingInfoDto nextBooking;
     private List<CommentDto> comments;
